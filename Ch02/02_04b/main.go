1232cede0447dde619b350a36e919e62f9e3f372
th3mon <p.szelenberger@gmail.com>
2026-01-15 20:56:25 +0100

    Play with Maths

--- Ch02/02_04b/main.go
+++ Ch02/02_04b/main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Math")
+	i1, i2, i3 := 5, 7, 99
+	sum1 := i1 + i2 + i3
+
+	fmt.Println("Int sum:", sum1)
+
+	f1, f2, f3 := 33.3, 0.4, 55.5
+	sum2 := f1 + f2 + f3
+
+	fmt.Println("Float sum:", sum2)
+
+	total := float64(sum1) + sum2
+
+	fmt.Println("Total is:", total)
 }
