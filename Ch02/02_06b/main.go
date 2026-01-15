a1df4714462f223e42460c928403e473f52849ad
th3mon <p.szelenberger@gmail.com>
2026-01-15 21:32:31 +0100

    Play with Date and Time

--- Ch02/02_06b/main.go
+++ Ch02/02_06b/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
+	t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
 
-	fmt.Println("Dates and times")
+	fmt.Printf("Go launched %v\n", t)
 
+	n := time.Now()
+	fmt.Println("Now", n)
+
+	fmt.Println(n.Format(time.ANSIC))
 }
