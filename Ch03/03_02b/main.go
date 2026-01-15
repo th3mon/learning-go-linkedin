f3bd781ccfc3ac369d0fdfdf31e1b8fd4d2bc8cb
th3mon <p.szelenberger@gmail.com>
2026-01-15 22:32:08 +0100

    Fun with pointers

--- Ch03/03_02b/main.go
+++ Ch03/03_02b/main.go
@@ -5,5 +5,21 @@ import (
 )
 
 func main() {
+	m := make(map[string]int)
+	m["jeden"] = 1
+	fmt.Println(m)
+
 	fmt.Println("Pointers")
+
+	number := 42
+	var p *int = &number
+	var p2 *int = &number
+
+	*p += *p2
+
+	if p == nil {
+		fmt.Println("p is nil")
+	} else {
+		fmt.Println(*p)
+	}
 }
