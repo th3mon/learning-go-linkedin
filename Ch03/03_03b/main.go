a97235a1ba722e94ee85d4330ef4266bf6d62e8b
th3mon <p.szelenberger@gmail.com>
2026-01-15 23:20:18 +0100

    Fun with slices

--- Ch03/03_03b/main.go
+++ Ch03/03_03b/main.go
@@ -5,5 +5,9 @@ import (
 )
 
 func main() {
-	fmt.Println("Arrays")
+	arr := []string{"jeden", "dwa"}
+	arr2 := make([]string, 0, 1)
+	arr2 = append(arr2, arr[1:]...)
+	fmt.Println(arr[0:])
+	fmt.Println(arr2)
 }
