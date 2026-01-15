4d953ad2c8d34dbdd5861434222756d014449567
th3mon <p.szelenberger@gmail.com>
2026-01-15 20:56:15 +0100

    Get user input

--- Ch02/02_03b/main.go
+++ Ch02/02_03b/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+
+	messge, _ := reader.ReadString('\n')
+	fmt.Println(messge)
+
+	messge, _ = reader.ReadString('\n')
+	number, err := strconv.ParseFloat(strings.TrimSpace(messge), 64)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("The number is:", number)
+	}
 }
