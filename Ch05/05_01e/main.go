aa82905d5765b033b783d0d5edaaca36480dd06c
th3mon <p.szelenberger@gmail.com>
2026-01-21 03:27:05 +0100

    Use named return values

--- Ch05/05_01e/main.go
+++ Ch05/05_01e/main.go
@@ -17,19 +17,18 @@ func doSomething() {
 	fmt.Printf("The sum is %v.\n", sum)
 	fmt.Printf("The count is %v.\n", count)
 	fmt.Printf("The average is %v.\n", average)
-
 }
 
 func addValues(value1, value2 int) int {
 	return value1 + value2
 }
 
-func addAllValues(values ...int) (int, int, float64) {
-	sum := 0
+func addAllValues(values ...int) (sum int, count int, average float64) {
+	sum = 0
 	for _, v := range values {
 		sum += v
 	}
-	count := len(values)
-	average := float64(sum) / float64(count)
+	count = len(values)
+	average = float64(sum) / float64(count)
 	return sum, count, average
 }
