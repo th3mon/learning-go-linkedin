5bc3e2f7b278c794ba94238acb6cfe86d083b390
th3mon <p.szelenberger@gmail.com>
2026-01-26 20:48:41 +0100

    Play with methods

--- Ch05/05_02b/main.go
+++ Ch05/05_02b/main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
 	dog := Dog{"Poodle", "Woof"}
 	fmt.Printf("The %v says %v!\n", dog.Breed, dog.Sound)
+
+	var n number = 8
+	fmt.Println(n.ToString())
 }
 
 type Dog struct {
-   Breed string
-   Sound string
+	Breed string
+	Sound string
+}
+
+type number int64
+
+func (n number) ToString() string {
+	return strconv.Itoa(int(n))
 }
